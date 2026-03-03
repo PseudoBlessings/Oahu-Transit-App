3a258bff2850d6b5a787f527359f75c66f9c5721
PsuedoBlessings <zmasike@gmail.com>
2026-03-02 18:48:06 -1000

    Reworked app.json to get expo to work

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -5,7 +5,7 @@ export default function Index() {
     <View
       className="flex-1 justify-center items-center"
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>I guess we are in!</Text>
     </View>
   );
 }
