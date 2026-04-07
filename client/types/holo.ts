f779c5f79c7ee8034bcc6a604118fb27b9b19f1b
PsuedoBlessings <zmasike@gmail.com>
2026-04-06 14:05:36 -1000

    Added Capping information to Holocard Capping Info

--- client/types/holo.ts
+++ client/types/holo.ts
@@ -1,9 +1,20 @@
+export interface HolocardCappingInfo{
+    Name: string;
+    ValidFrom: string; // ISO 8601 Date string
+    ValidTo: string;   // ISO 8601 Date string
+    Amount: number;
+    CurrentValue: number;
+    MissingValue: number;
+    Description: string;
+}
+
 export interface HolocardInfo{
     cardId: number
     cardPhyscialID: string
     cardType: string
     cardName: string
     balance: number
+    holocardCappingInfo: HolocardCappingInfo[] | undefined
     currentPass:string
 }
 
